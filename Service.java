21bff5c4bd3a68adf4e7acedf64c818fb304cbba
vamsipavuluri <vamsi19051998@gmail.com>
2021-10-13 10:35:54 +0530

    new file is added into genpact2

--- Service.java
+++ Service.java
@@ -7,3 +7,12 @@
     i = 5;
     System.out.println("Constructor is called");
   }
+# new methos is adding on the genpact2
+class Main {
+  public static void main(String[] args) {
+
+    // object is created in another class
+    Company obj = new Company();
+    System.out.println("Company name = " + obj.name);
+  }
+}
